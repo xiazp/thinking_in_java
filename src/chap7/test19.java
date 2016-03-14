cf043a85156f47e34fd67a1acd9eecd7978cd03b
justin <xiazhiping@foxmail.com>
2016-03-14 08:18:13 +0800

    pull bak

---
+++ src/chap7/test19.java
@@ -0,0 +1,20 @@
+package chap7;
+
+class t19{
+	public final int i;
+	public final String s;
+	
+	t19(){
+		i = 1;
+		s = "123";
+	}
+	
+	t19(int i){
+		this.i = i;
+		s = "321";
+	}
+}
+
+public class test19 {
+	public static void main()
+}
