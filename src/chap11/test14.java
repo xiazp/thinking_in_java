2520aed115a3e57e7a7966045e32b0fedc7d58cd
justin <xiazhiping@foxmail.com>
2016-03-21 09:19:31 +0800

    add chap11-14 but not sync with home

--- src/chap11/test14.java
+++ src/chap11/test14.java
@@ -6,18 +6,24 @@ import java.util.ListIterator;
 public class test14 {
 	public static void main(String[] args){
 		LinkedList<Integer> ll = new LinkedList<Integer>();
-		ListIterator it;
+		ListIterator<Integer> it;
 		
-		for(int i=0; i<6; i++){
+		for(int i=0; i<4; i++){
 			ll.add(i, i);
 		}
 		System.out.println("ll: " + ll);
 		
 		
-		for(int i=0; i<3; i++){
-			ll.add(i * 2, 8); 
+		it = ll.listIterator();
+		while(it.hasNext() ){
+			
+			it.add(9);
+			System.out.println("cur:" + it.next() );
+			//it.next();
 		}
 		System.out.println("ll: " + ll);
+		
+		
 		
 	}
 }
