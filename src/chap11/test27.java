7c17ec8fea7b252d31c8c7305b85ad69d2270329
xiazp <xiazhiping@foxmail.com>
2016-03-22 23:01:39 +0800

    1.add chap11 test27 test28

--- src/chap11/test27.java
+++ src/chap11/test27.java
@@ -1,5 +1,52 @@
 package chap11;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Command{
+	String s;
+	public void operation(){
+		System.out.println("operation(): " + s);
+	}
+	
+	public Command(String s){
+		this.s = s;
+	}
+}
+
+class QueueA{
+	Queue<Command> qe = new LinkedList<Command>();
+	
+	public Command setMember(Command q){
+		qe.offer(q);
+		return q;
+	}
+	
+	public Queue<Command> get(){
+		return qe;
+	}
+}
+
+
 public class test27 {
+	
+	public static void printQ(Queue<? extends Command> q){
+		while(!q.isEmpty()){
+			Command c = q.poll();
+			c.operation();
+		}
+	}
+
+	public static void main(String[] args){
+		QueueA qa = new QueueA();
+		
+		qa.setMember(new Command("c1"));
+		qa.setMember(new Command("c2"));
+		qa.setMember(new Command("c3"));
+		
+		printQ(qa.get());
+		
+	}
+
 
 }
--- src/chap11/test28.java
+++ src/chap11/test28.java
@@ -1,5 +1,24 @@
 package chap11;
 
-public class test28 {
+import java.util.PriorityQueue;
+import java.util.Random;
 
+public class test28 {
+	public static void main(String[] args){
+		PriorityQueue<Double> pq = new PriorityQueue<Double>();
+		Random rand = new Random(47);
+		
+		for(int i =0; i < 6; i++){
+			pq.offer(rand.nextDouble());
+		}
+		System.out.println("pq: " + pq);
+		
+		while(!pq.isEmpty()){
+			System.out.println("d: " + pq.poll());
+		}
+		
+		
+	}
+
+	
 }
