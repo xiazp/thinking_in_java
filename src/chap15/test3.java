105fb74213765df52fd78450082ac5f7779fb5a7
justin <xiazhiping@foxmail.com>
2016-04-05 11:19:31 +0800

    add chap15 3~4

--- src/chap15/test3.java
+++ src/chap15/test3.java
@@ -1,9 +1,42 @@
 package chap15;
 
+class TwoTuple<A, B>{
+	public final A first;
+	public final B second;
+	
+	public TwoTuple(A a, B b){
+		first = a;
+		second = b;
+	}
+	
+	public String toString(){
+		return ("( " + first + " " + second+" )");
+	}
+}
+
+
+class ThreeTuple<A, B, C> extends TwoTuple<A,B>{
+	public final C third;
+	
+	public ThreeTuple(A a,B b,C c){
+		super(a,b);
+		third = c;
+	}
+	
+	public String toString(){
+		return ("( " + first + " " + second + " " + third +" )");
+	}
+}
+
 public class test3 {
 		
 	public static void main(String[] args){
+		TwoTuple<Integer, String> tw = new TwoTuple<Integer, String>(3, "sdf");
+		ThreeTuple<Integer, String, Integer> tr = 
+				new ThreeTuple<Integer, String, Integer>(2, "three", 23);
 		
+		System.out.println(tw.toString());
+		System.out.println(tr.toString());
 	}
 
 }
--- src/chap15/test4.java
+++ src/chap15/test4.java
@@ -1,9 +1,59 @@
 package chap15;
 
-public class test4 {
-		
-	public static void main(String[] args){
-		
-	}
+import java.lang.reflect.Array;
 
+// "·ºÐÍ»¯sequence"
+class Sequence<T>{
+	private T[] item;
+	private int next= 0;
+	
+	Sequence( int i){
+		 item = (T[])new Object[i];
+	}
+	
+	Sequence(Class<T> c, int i){
+		//item = (T[])new Object[i];
+       @SuppressWarnings("unchecked")
+        final T[] a = (T[]) Array.newInstance(c, i);
+        this.item = a;
+	}
+	
+	public void add(T x){
+		if(next < item.length)
+			item[next++]=x;
+	}
+	
+	public T get(int i){
+		if(i < item.length )
+			return item[i];
+		else
+			return null;
+	}
+}
+
+class Item{
+	public String s = "null";
+	
+	public String toString(){
+		return s;
+	}
+}
+
+
+public class test4 {
+
+	public static void main(String[] args){
+		Sequence<Item> sq = new Sequence<Item>(10);
+		
+		for(int i=0; i < 5; i++){
+			Item item = new Item();
+			item.s = Integer.toString(i);
+			sq.add(item);
+		}
+		
+		for(int i=0; i < 5; i++){
+			Item it = sq.get(i);
+			System.out.println(it.toString());
+		}	
+	}
 }
