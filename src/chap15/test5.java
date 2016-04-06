ad2433bef579d887b71cc394a0fa17b99dba1bb1
justin <xiazhiping@foxmail.com>
2016-04-06 10:32:25 +0800

    add chap15 5~6

--- src/chap15/test5.java
+++ src/chap15/test5.java
@@ -1,8 +1,56 @@
 package chap15;
 
+class LinkedStack<T> {
+	private  class Node{
+		T item;
+		Node next;
+		
+		//constructor
+		Node(){
+			item = null; 
+			next = null;
+		}
+	
+		//constructor
+		Node(T item, Node next){
+			this.item = item;
+			this.next = next;
+		}
+		
+		//stack is null
+		boolean end(){
+			return item == null && next == null;
+		}
+	}
+	
+	private Node top = new Node();
+	
+	public void push(T item){
+		top = new Node(item, top);
+	}
+	
+	public T pop(){
+		T result = top.item;
+		if(!top.end())
+			top = top.next;
+		return result;
+	}
+	
+}
+
 public class test5 {
 		
 	public static void main(String[] args){
+		LinkedStack<String> ls = new LinkedStack<String>();
+		
+		for(String s: "12 23 34 23 34 34".split(" ")){
+			ls.push(s);
+		}
+		
+		String el;
+		while( ( el= ls.pop()) != null ){
+			System.out.println(el);
+		}
 		
 	}
 
--- src/chap15/test6.java
+++ src/chap15/test6.java
@@ -1,9 +1,32 @@
 package chap15;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+class RandomList<T>{
+	private ArrayList<T> storage = new ArrayList<T>();
+	private Random rand = new Random(47);
+	
+	public void add(T item){
+		storage.add(item);
+	}
+	
+	public T selecte(){
+		return storage.get(rand.nextInt(storage.size()));
+	}
+	
+}
+
 public class test6 {
 		
 	public static void main(String[] args){
+		RandomList<Integer> rl = new RandomList<Integer>();
 		
+		for(String s: "1 12 33 2 3 4 5 6 ".split(" "))
+			rl.add(Integer.valueOf(s));
+		
+		for(int i=0; i<10; i++)
+			System.out.println(rl.selecte());
 	}
 
 }
