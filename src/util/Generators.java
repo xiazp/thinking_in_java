73266a56d25c5cde856e0682c3b3c78de874e515
justin <xiazhiping@foxmail.com>
2016-04-26 10:12:41 +0800

    add chap18 1

---
+++ src/util/Generators.java
@@ -0,0 +1,26 @@
+package util;
+
+import java.util.Collection;
+
+
+
+public class Generators{
+	public static <T> Collection<T> fill( Collection<T> t, Generator<T> gen,
+			 int n){
+		
+		for(int i =0; i < n; i++){
+			t.add(gen.next());
+		}
+		
+		return t;
+	}
+	
+//	public static <T> ArrayList<T>
+//	fill(ArrayList<T> arrayList, Generator<T> generator, int n) {
+//	  for(int i = 0; i < n; i++)
+//	    arrayList.add(generator.next());
+//	  return arrayList;
+//	}	
+}
+
+
